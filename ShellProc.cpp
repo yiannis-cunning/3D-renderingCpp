d1a9b4873f4ca3957ced75a65e2b26d1e820445a
yiannis-cunning <98997501+yiannis-cunning@users.noreply.github.com>
2022-02-04 01:17:39 -0500

    Add files via upload

---
+++ ShellProc.cpp
@@ -0,0 +1,42 @@
+#include "windClass.h"
+
+
+
+void paint(HWND hwnd, struct StateInfoMain *pState){
+       PAINTSTRUCT ps;
+	HDC hdc = BeginPaint(hwnd, &ps);
+
+       const COLORREF rgbRed   =  0x00FFFFFF;
+	HBRUSH h = CreateSolidBrush(rgbRed);
+
+	FillRect(hdc, &ps.rcPaint, h);
+       EndPaint(hwnd, &ps);
+}
+
+
+LRESULT CALLBACK ShellWindow::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{	
+	struct StateInfoMain *pState;
+
+       if(uMsg == WM_CREATE){
+		CREATESTRUCT *pCreate = (CREATESTRUCT *)(lParam);
+		pState = (struct StateInfoMain*)(pCreate->lpCreateParams);
+		SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pState);
+
+       }
+
+       else{pState = (struct StateInfoMain *)GetWindowLongPtr(hwnd, GWLP_USERDATA);;}
+	
+	switch(uMsg){
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return(0);
+              case WM_PAINT:
+                     paint(hwnd, pState);
+              	return 0;
+
+
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
---
+++ shell.cpp
@@ -0,0 +1,56 @@
+#include "windClass.h"
+
+// -lstdc++
+// gcc ShellWindow.cpp ChildWindow.cpp shell.cpp -ld2d1 -lgdi32 -lstdc++ -o run
+// gcc *.cpp -ld2d1 -lgdi32 -lstdc++ -o run
+
+// Windows initialzation
+//
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+
+
+	static TCHAR name[] = TEXT ("WindowThing");
+	int size = 11;
+
+	// Create shell window
+	ShellWindow shell(hInstance, name, size);
+	int h = shell.createWnd(hInstance, nCmdShow);
+
+	// Create child window
+	ChildWindow canvas(hInstance);
+	canvas.registerWnd();
+	canvas.createWnd(shell.getHwnd(), hInstance, nCmdShow);
+
+
+
+
+
+
+	if (h == 0){return 0;}	
+	// set up path of camera
+	int i = 0;
+	double theta = 0;
+	float v[3] = {0,0,0};
+	float r = sqrt(20*20 + 10*10);
+	theta = 0.5;
+	setVector(v, r*cos(theta),r*sin(theta),r*cos(theta));
+	canvas.changelens(v);
+	
+	MSG msg = {};
+	while(GetMessage(&msg, NULL, 0, 0)){
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+		
+		if(i == 1){
+			i = 0;
+			theta = theta += 0.01;
+			setVector(v, r*cos(theta),r*sin(theta),r*cos(theta));
+			canvas.changelens(v);
+		}
+		i++;
+	}
+	
+    	return 0;
+}
+
+
